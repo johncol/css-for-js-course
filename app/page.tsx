507f6b430a8c27d11405bca9624e2b31830c8dc1
John <john.19col@gmail.com>
2023-12-22 12:05:08 -0800

    Add global styles using styled-components

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,23 @@
+"use client";
+
+import { GlobalStyles } from "./GlobalStyles";
 import { Quote } from "./Quote";
 
 const Home = () => {
   return (
-    <main>
-      <Quote
-        by="Academy Professor in the book If We Were Villains by M. L. Rio"
-        source="https://www.cloudlakeliterary.ca/blogposts/book-review-if-we-were-villains-by-m-l-rio"
-      >
-        If you have not made any enemies in life, you&apos;ve been living too
-        safely
-      </Quote>
-    </main>
+    <>
+      <main>
+        <Quote
+          by="Academy Professor in the book If We Were Villains by M. L. Rio"
+          source="https://www.cloudlakeliterary.ca/blogposts/book-review-if-we-were-villains-by-m-l-rio"
+        >
+          If you have not made any enemies in life, you&apos;ve been living too
+          safely
+        </Quote>
+      </main>
+
+      <GlobalStyles />
+    </>
   );
 };
 
